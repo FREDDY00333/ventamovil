923d92129cfe6770c66891edb7d5ff8ec69d5d98
FREDDY00333 <30426441+FREDDY00333@users.noreply.github.com>
2022-08-21 10:56:57 -0400

    VentaMovil

---
+++ android/app/src/main/kotlin/com/example/curiosity/MainActivity.kt
@@ -0,0 +1,6 @@
+package ventamovil.cl
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/example/ventamovil/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ventamovil
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
